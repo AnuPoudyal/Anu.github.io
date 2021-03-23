5a3e1af3457350cee2693d75744b49ec34fce9cd
AnuPoudyal <79480470+AnuPoudyal@users.noreply.github.com>
2021-03-22 20:56:11 -0500

    w1d1_solution

--- CS303/labW1D1/app.js
+++ CS303/labW1D1/app.js
@@ -3,7 +3,7 @@
 "use strict";
 
 function pow(x, n) {
-	return x * n;
+	return x ** n;
 }
 
 function add2(n1, n2) {
@@ -29,7 +29,22 @@ function add(...args) {
 	60 - 69  D
 	0 - 59   NC
 */
-function computeGrade(grades) {
-	return "???";
-}
+
+	function computeGrade(grades) {
+		for (let grade of grades) {
+		  if (grade >= 90 && grade <= 100) {
+			return "A";
+		  } else if (grade >= 80) {
+			return "B";
+		  } else if (grade >= 70) {
+			return "C";
+		  } else if (grade >= 60) {
+			return "D";
+		  } else if (grade < 60 && grade >= 0) {
+			return "NC";
+		  }
+		}
+	  }
+	  module.exports = { pow, add2, add, computeGrade };
+
 
